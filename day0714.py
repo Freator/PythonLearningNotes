151ad6533be096eaaddd6d5999547d5b353c0e02
Freator <tbcoong@qq.com>
2018-12-11 18:24:52 +0800

    add a python code file

---
+++ day0714.py
@@ -0,0 +1,9 @@
+print("------------Freator---------------")
+temp = input("guess the number in my mind:")
+guess = int(temp)
+if guess == 8:
+    print("RIGHT!")
+    print("But no prize!")
+else:
+    print("WRONG!")
+print("Game Over!")
