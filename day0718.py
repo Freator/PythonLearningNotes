70ce1c4985ef0a4796edd43b499eefd9851ae4cf
Freator <tbcoong@qq.com>
2018-12-15 17:16:52 +0800

    Add a new python code file <day0718>

---
+++ day0718.py
@@ -0,0 +1,30 @@
+﻿# -*- coding: utf-8 -*-
+'''
+def product(x,*y):
+	count = 1
+	for i in y:
+		count = count * i
+	return count * x
+print(product(1))
+try:
+	product(3)
+	print("ERROR")
+except TypeError:
+	print("RIGHT")
+'''
+'''
+# hanoi algoorithm
+def move(n,a,b,c):
+	if n == 1:
+		print(a,'-->',c)
+	else:
+		move(n-1,a,c,b)
+		move(1,a,b,c)
+		move(n-1,b,a,c)
+move(3,'A','B','C')
+'''
+d = {'A':1,'B':2,'C':3}
+for q in d.values():
+	print(q)
+
+
