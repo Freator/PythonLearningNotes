e334274a5a8ed9de74bc9a6c9c805331ab6005b4
Freator <tbcoong@qq.com>
2018-12-21 11:06:17 +0800

    Add a new python code file <day0726>

---
+++ day0726.py
@@ -0,0 +1,16 @@
+﻿# -*- coding: utf-8 -*-
+'a test module'
+__author = 'Freator'
+
+import sys
+
+def test():
+	args = sys.argv
+	if len(args) == 1:
+		print('hello world')
+	elif len(args) == 2:
+		print('Hello, %s' %args[1])
+	else:
+		print("Too many arguments")
+if __name__ == '__main__':
+	test()
